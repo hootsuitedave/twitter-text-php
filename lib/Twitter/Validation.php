72205c2588cea826e8c1f9ccb0a7a8c44af7b0ee
davidr <david.russell@hootsuite.com>
2015-11-11 00:53:42 -0800

    all links are counted as 23 characters, add gTLDs

--- lib/Twitter/Validation.php
+++ lib/Twitter/Validation.php
@@ -36,7 +36,7 @@ class Twitter_Validation extends Twitter_Regex {
    *
    * @var  int
    */
-  protected $short_url_length = 22;
+  protected $short_url_length = 23;
 
   /**
    * The length of a short URL beginning with http:
